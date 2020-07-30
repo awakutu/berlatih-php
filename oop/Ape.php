80563ab586affecfa4fe8d68f48f796841efedb9
awakutu <rizkytrisulistyo@gmail.com>
2020-07-31 01:23:32 +0700

    add OOP

---
+++ oop/Ape.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'Animal.php';
+
+class Ape extends Animal {
+	function yell(){
+		echo "Auooo<br>";
+	}
+}
+
+?>
---
+++ oop/frog.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'Animal.php';
+
+class Frog extends Animal {
+
+	function jump(){
+		echo "hop hop <br>";
+	}
+
+}
+
+?>
